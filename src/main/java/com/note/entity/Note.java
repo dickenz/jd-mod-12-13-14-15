d28b180cf6acc3ca06656e49b535fe62407fabd4
Ira <37289156+dickenz@users.noreply.github.com>
2023-09-15 18:50:19 +0300

    Update Note.java

--- src/main/java/com/note/entity/Note.java
+++ src/main/java/com/note/entity/Note.java
@@ -1,12 +1,17 @@
 package com.note.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
-import java.util.UUID;
-
+@Entity
 @Data
 public class Note {
-    private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String content;
 
