00899c53ddba40d88d1fad7eff631bbaa71d5fb2
Ira <37289156+dickenz@users.noreply.github.com>
2023-09-15 18:52:34 +0300

    Update NoteService.java

--- src/main/java/com/note/service/NoteService.java
+++ src/main/java/com/note/service/NoteService.java
@@ -1,7 +1,9 @@
 package com.note.service;
 
+import com.note.data.NoteRepository;
 import com.note.entity.Note;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -9,50 +11,44 @@ import java.util.*;
 @Service
 public class NoteService {
 
-    private final Map<UUID, Note> notes = new HashMap<>();
+    private final NoteRepository noteRepository;
 
-    public List<Note> listAll() {
-        return new ArrayList<>(notes.values());
+    @Autowired
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
     }
 
-    public Note add(Note note) {
-        UUID id = UUID.randomUUID();
-        note.setId(id);
-        notes.put(id, note);
-        return note;
-    }
-
-    public void deleteById(UUID id) {
-        if (!notes.containsKey(id)) {
-            throw new IllegalArgumentException("Note with id " + id + " not found");
-        }
-        notes.remove(id);
-    }
-
-    public void update(Note note) {
-        UUID id = note.getId();
-        if (!notes.containsKey(id)) {
-            throw new IllegalArgumentException("Note with id " + id + " not found");
-        }
-        notes.put(id, note);
-    }
-
-    public Note getById(UUID id) {
-        if (!notes.containsKey(id)) {
-            throw new IllegalArgumentException("Note with id " + id + " not found");
-        }
-        return notes.get(id);
-    }
-
-    public NoteService() {
+    public void initializeNotes() {
         Note note1 = new Note();
         note1.setTitle("Sample Note 1");
         note1.setContent("This is the content of note 1.");
-        add(note1);
+        noteRepository.save(note1);
 
         Note note2 = new Note();
         note2.setTitle("Sample Note 2");
         note2.setContent("This is the content of note 2.");
-        add(note2);
+        noteRepository.save(note2);
     }
+
+    public List<Note> listAll() {
+        return noteRepository.findAll();
+    }
+
+    public Note add(Note note) {
+        return noteRepository.save(note);
+    }
+
+    public void deleteById(Long id) {
+        noteRepository.deleteById(id);
+    }
+
+    public void update(Note note) {
+        noteRepository.save(note);
+    }
+
+    public Note getById(Long id) {
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Note with id " + id + " not found"));
+    }
+
 }
