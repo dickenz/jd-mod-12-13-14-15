4334eee293dd2b998e6d290976386159adc4063d
Ira <37289156+dickenz@users.noreply.github.com>
2023-09-19 11:27:59 +0300

    Update V1__create_note_table.sql

--- src/main/resources/db/migration/V1__create_note_table.sql
+++ src/main/resources/db/migration/V1__create_note_table.sql
@@ -3,3 +3,7 @@ CREATE TABLE note (
     title VARCHAR(255) NOT NULL,
     content TEXT
 );
+
+INSERT INTO note (title, content) VALUES
+    ('Sample Note 1', 'This is the content of note 1.'),
+    ('Sample Note 2', 'This is the content of note 2.');
